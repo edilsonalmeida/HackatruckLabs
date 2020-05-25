b02ec03830d8d1bb82ea10b42ad5bdd281b1425a
Edilson <Edilson.almeida@eldorado.org.br>
2020-05-25 18:19:57 -0300

    initial Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$.fn.parallax = function(resistance, mouse) {
+    $el = $(this);
+    TweenLite.to($el, 0.2, {
+      x: -((mouse.clientX - window.innerWidth / 2) / resistance),
+      y: -((mouse.clientY - window.innerHeight / 2) / resistance)
+    });
+  };
+  
+  $(document).mousemove(function(e) {
+    $(".background").parallax(-30, e);
+    $(".hacka-big").parallax(30, e);
+    $(".hacka-banner h1").parallax(10, e);
+    });
